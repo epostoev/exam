5145773982330bcd5af6b778f8601b517c187a7e
Your Name <you@example.com>
2026-01-16 18:13:33 +0300

    stepic/9.2_strings.c

--- stepic/9.2_strings.c
+++ stepic/9.2_strings.c
@@ -17,8 +17,18 @@ void print_array(char* string_1, int size_string_1) {
 }
 
 int main (void) {
-	char string_1[] = "Hello World";
+	char string_1[] = "Hello/* World";
 	int size_string_1 =  strlen(string_1);
 	print_array(string_1, size_string_1);
+	int i = 0;
+	int flag = 0;
+	while(i < size_string_1) {
+		if (string_1[i] == '/' && string_1[i + 1] == '*') {
+			flag = 1;
+			i++;
+			getchar();
+		}
+		i++;
+	}
 	return 0;
 }
