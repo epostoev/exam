ef679d3d90fc03e9cc0d0d3813e1f508ed04643c
Lharys Endon <lharysen@il-d6.msk.21-school.ru>
2026-02-26 21:26:42 +0300

    03

---
+++ 03/03_01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main() { double r = 0, m = 0, V =0;
+    int res = scanf("%lf", &r);
+    char last_char = getchar();
+    if (res == 1 && last_char == '\n'){V = (4.0 / 3.0) * M_PI * pow(r, 3);
+        printf("%.0lf", m = 21500 * V);
+    }
+    else { printf("n/a");
+    }
+    return 0;
+}
