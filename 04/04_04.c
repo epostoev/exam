d393a930e93caa3e0f3c64ba43fe08f237316774
Postoev <e.postoev@yandex.ru>
2026-02-27 09:54:06 +0300

    add 04

---
+++ 04/04_04.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int number_i = 0;
+    int unit_of_number = 0;
+    int max = 0;
+    int chk_number_i = scanf("%d", &number_i);
+    char last_symbol_c = getchar();
+
+    if (chk_number_i != 1 && last_symbol_c != '\n') {
+        printf("n/a");
+        return 0;
+    }
+    else {
+        number_i = abs(number_i);
+        for(;number_i > 0;) {
+            unit_of_number = number_i % 10;
+            if (max < unit_of_number ) {
+            max = unit_of_number;
+            }
+            number_i = number_i / 10;
+        }    
+    }
+    printf("%d\n", max);
+}
